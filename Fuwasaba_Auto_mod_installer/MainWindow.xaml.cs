faf7712d0f2e6e1eacff4ee075f0aaa2538c6adf
Quantum8060 <akaunnt8060@gmail.com>
2024-06-01 17:04:21 +0900

    プロジェクト ファイルを追加します。

---
+++ Fuwasaba_Auto_mod_installer/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Security.Principal;
+using Microsoft.Win32;
+
+
+namespace Fuwasaba_Auto_mod_installer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath = @$"C:\Users\{Environment.UserName}\AppData\Roaming\.minecraft\versions\1.20.1-forge-47.2.32";
+
+            if (Directory.Exists(filePath))
+            {
+                OpenFolderDialog dialog = new OpenFolderDialog()
+                {
+                    Title = "フォルダ選択",
+                    InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                    Multiselect = false
+                };
+
+                string folderName = "";
+                if (dialog.ShowDialog() == true)
+                {
+                    folderName = dialog.FolderName;
+                    MessageBox.Show(folderName);
+
+                }
+
+            }
+            else
+            {
+                System.Diagnostics.Process.Start(@"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe", "https://adfoc.us/serve/sitelinks/?id=271228&url=https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-47.2.32/forge-1.20.1-47.2.32-installer.jar");
+                MessageBox.Show("1.20.1 Forge 47.2.32が存在しません!\nダウンロードしてください。");
+            }
+        }
+    }
+}
