0e87b4116c19fbeaadd979d8dece86815ca39393
Quantum8060 <akaunnt8060@gmail.com>
2024-06-16 00:15:45 +0900

    メモリ割り当ての変更機能を追加

--- Fuwasaba_Auto_mod_installer/MainWindow.xaml.cs
+++ Fuwasaba_Auto_mod_installer/MainWindow.xaml.cs
@@ -47,8 +47,9 @@ namespace Fuwasaba_Auto_mod_installer
 
                 MessageBox.Show("構成を作成しています。");
 
-                System.Threading.Thread.Sleep(10000);
+                System.Threading.Thread.Sleep(7000);
 
+                File.Delete($@"{folderName}\fuwasaba.zip");
                 File.Move(@$"{Famifolder}\fuwasaba.zip", @$"{folderName}\fuwasaba.zip");
 
                 string zipPath = @$"{folderName}\fuwasaba.zip";
@@ -69,6 +70,7 @@ namespace Fuwasaba_Auto_mod_installer
                 }
 
                 string jsonText = await File.ReadAllTextAsync(launcherPath);
+                string ram_slider = ramSlider.Value.ToString();
                 JObject launcherProfiles = JObject.Parse(jsonText);
 
                 JObject newProfile = new JObject
@@ -80,7 +82,7 @@ namespace Fuwasaba_Auto_mod_installer
                     ["type"] = "custom",
                     ["icon"] = "Grass",
                     ["gameDir"] = folderName,
-                    ["javaArgs"] = "-Xmx4G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M"
+                    ["javaArgs"] = $"-Xmx{ram_slider}G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M"
                 };
 
                 JObject profiles = (JObject)launcherProfiles["profiles"];
@@ -101,81 +103,9 @@ namespace Fuwasaba_Auto_mod_installer
             }
         }
 
-        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        private void ramSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            string filePath = @$"C:\Users\{Environment.UserName}\AppData\Roaming\.minecraft\versions\1.20.1-forge-47.2.32";
-
-            if (Directory.Exists(filePath))
-            {
-                OpenFolderDialog dialog = new OpenFolderDialog()
-                {
-                    Title = "フォルダ選択",
-                    InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
-                    Multiselect = false
-                };
-
-                string folderName = "";
-                if (dialog.ShowDialog() == true)
-                {
-                    folderName = dialog.FolderName;
-                }
-
-                var app = new ProcessStartInfo();
-
-                app.FileName = "download.exe";
-
-                Process.Start(app);
-
-                MessageBox.Show("構成を作成しています。");
-
-                string zipPath = @$"{folderName}\fuwasaba.zip";
-                string extractPath = @$"{folderName}";
-
-                await Task.Run(() => ZipFile.ExtractToDirectory(zipPath, extractPath));
-                File.Delete(zipPath);
-
-                string launcherPath = System.IO.Path.Combine(
-                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
-                    ".minecraft", "launcher_profiles.json"
-                );
-
-                if (!File.Exists(launcherPath))
-                {
-                    MessageBox.Show("launcher_profiles.jsonが見つかりませんでした。");
-                    return;
-                }
-
-                string jsonText = await File.ReadAllTextAsync(launcherPath);
-                JObject launcherProfiles = JObject.Parse(jsonText);
-
-                JObject newProfile = new JObject
-                {
-                    ["name"] = "ふわ鯖1.5期専用軽量化構成",
-                    ["lastVersionId"] = "1.20.1-forge-47.2.32",
-                    ["created"] = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fffZ"),
-                    ["lastUsed"] = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fffZ"),
-                    ["type"] = "custom",
-                    ["icon"] = "Grass",
-                    ["gameDir"] = folderName,
-                    ["javaArgs"] = "-Xmx4G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M"
-                };
-
-                JObject profiles = (JObject)launcherProfiles["profiles"];
-                profiles["MyNewProfile"] = newProfile;
-
-                await File.WriteAllTextAsync(launcherPath, launcherProfiles.ToString());
-
-                MessageBox.Show("構成を作成しました。");
-
-                System.Diagnostics.Process.Start(@"C:\XboxGames\Minecraft Launcher\Content\Minecraft.exe");
-
-                this.Close();
-            }
-            else
-            {
-                System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe", "https://adfoc.us/serve/sitelinks/?id=271228&url=https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-47.2.32/forge-1.20.1-47.2.32-installer.jar");
-                MessageBox.Show("1.20.1 Forge 47.2.32が存在しません!\nダウンロードしてください。");
-            }
+            
         }
     }
 }
